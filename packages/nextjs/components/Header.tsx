42a2bbdaf37a58668a2d050f1c9b1658da2e3f6c
Paul Gadi <paulgadi@gmail.com>
2023-08-05 23:20:56 -0400

    replace boilerplate text and images

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -90,8 +90,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Soul Cats 😽</span>
+            <span className="text-xs">Spreading 💖LOVE💖 across the Superchain ⛓️</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -14,9 +14,9 @@ type MetaHeaderProps = {
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
-  title = "Scaffold-ETH 2 App",
-  description = "Built with 🏗 Scaffold-ETH 2",
-  image = "thumbnail.jpg",
+  title = "Soul Cats 😽",
+  description = "Spreading 💖LOVE💖 across the Superchain ⛓️",
+  image = "thumbnail.png",
   twitterCard = "summary_large_image",
   children,
 }: MetaHeaderProps) => {
