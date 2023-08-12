9e771de01c0e000391973ae435c20cf31565667e
Paul Gadi <paulgadi@gmail.com>
2023-08-12 00:44:54 -0400

    add frontpage text

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -90,8 +90,7 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Soul Cats 😽</span>
-            <span className="text-xs">Spreading 💖LOVE💖 across the Superchain ⛓️</span>
+            <span className="font-bold leading-tight">SoulCats</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { BoltIcon, GiftIcon, HeartIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 import Image from "next/image";
 
@@ -17,46 +17,49 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Adopt Your Soulcat</span>
-            <span className="block text-4xl font-bold">Soulcats</span>
+            <span className="block text-4xl font-bold">SoulCats 😽</span>
+            <span className="block text-2xl mb-2">Spreading 💖LOVE💖 across the Superchain ⛓️</span>
           </h1>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <div className="flex-grow w-full mt-2 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <BoltIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
+                
+                Don't miss your chance to <b>Adopt a SoulCat</b> when the{" "} 
+                <b className="rainbow-text">Portal</b> is active!<br/><br/>
+                Only a few cross over every time. Each wallet can only adopt a SoulCat once.
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
+              <GiftIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
+                When a SoulCat crosses over, it brings over two gifts, one in each hand.<br/><br/>
+                There are 4 types of gifts: <br/>
+                <b className="care">Care</b>{", "}
+                <b className="respect">Respect</b>{", "}
+                <b className="responsibility">Responsibility</b>{", and "}
+                <b className="knowledge">Knowledge</b>{"."}
+                <br/><br/>Trade gifts with other SoulCat owners to complete the set!
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <HeartIcon className="h-8 w-8 fill-secondary" />
               <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
+                Collecting all 4 gifts will allow you to create <br/>
+                💖<b className="love-text">LOVE</b>💖.<br/><br/>
+                💖<b className="love-text">LOVE</b>💖 can be used to adopt a new SoulCat{" "} 
+                <b>even if the <b className="rainbow-text">Portal</b> is closed</b>.
               </p>
             </div>
           </div>
         </div>
+
+        
       </div>
+      
     </div>
   );
 };
